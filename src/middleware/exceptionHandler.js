b61a88100ef4f58a338a877ecfdb4ec30e6595d0
dcnis <bgk.dennis@yahoo.de>
2022-03-13 20:22:49 +0800

    cleaner syntax

--- src/middleware/exceptionHandler.js
+++ src/middleware/exceptionHandler.js
@@ -4,7 +4,7 @@ const exceptionHandler = {};
 
 exceptionHandler.handleException = (error, req, res, next) => {
     console.error(error);
-    const httpStatus = error.status ? error.status : 500;
+    const httpStatus = error.status || 500;
     const errorResponse = {};
     errorResponse.message = error.message;
     res.status(httpStatus).json(errorResponse);
