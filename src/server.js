3a9ed97f9cbccb03b0b67a7c10511fa1466dd75b
dcnis <bgk.dennis@yahoo.de>
2022-03-14 17:46:33 +0800

    added https

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
-import http from 'http';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import rootDir from './util/path.js';
+
 import app from './app.js';
 import { PORT } from './config/env.js';
 import log from './config/winston.js';
 
-const server = http.createServer(app)
 
-server.listen(PORT, () => log.info('server started listening on port:' + PORT));
+const privateKey = fs.readFileSync(path.join(rootDir, '..', 'cert', 'server.key'));
+const certificate = fs.readFileSync(path.join(rootDir, '..', 'cert', 'server.cert'));
+
+https.createServer(
+    {   key: privateKey,
+        cert: certificate
+    }, app)
+    .listen(PORT, () => {
+        log.info('server started on https://localhost:' + PORT);
+});
