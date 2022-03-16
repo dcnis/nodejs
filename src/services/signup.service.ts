21d9a870bda95307be279d2936bee8512a6610cc
dcnis <bgk.dennis@yahoo.de>
2022-03-16 10:53:42 +0800

    fixed email signup again

--- src/services/signup.service.ts
+++ src/services/signup.service.ts
@@ -8,17 +8,18 @@ import RedisClient from './redis.service.js';
 import SignupBody from '../models/signup-body.model.js';
 import log from '../config/winston.js';
 
-// create reusable transporter object using the default SMTP transport
-const transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
-  port: 587,
-  auth: {
-    user: 'jamel.bode29@ethereal.email',
-    pass: 'Z627AVkj16VzaewtZG',
-  },
-});
-
 export default class SignupService {
+  // create reusable transporter object using the default SMTP transport
+  // Login at: https://ethereal.email/login
+  private static transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'jamel.bode29@ethereal.email',
+      pass: 'Z627AVkj16VzaewtZG',
+    },
+  });
+
   public static signup(signupData: SignupBody, res: Response) {
     // Create Token with encrypt
     crypto.randomBytes(32, (err, buffer) => {
@@ -45,16 +46,18 @@ export default class SignupService {
         RedisClient.expire(key, 1200);
       });
 
-      // transporter.sendMail({
-      //     from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      //     to: "bgk.dennis@yahoo.de", // list of receivers
-      //     subject: "Verify your email✔", // Subject line
-      //     html: `<b>Hello Dennis! <br>
-      //     Please click on following link to verify your email: <a href="http://localhost:3000/signupVerification/${token}">
-      //     http://localhost:3000/signupVerification/${token}</a></b>`,
-      // }).catch((error) => {
-      //     console.log('Fehler beim Senden der Email ' + error)
-      // });
+      this.transporter
+        .sendMail({
+          from: '"Fred Foo 👻" <foo@example.com>', // sender address
+          to: 'bgk.dennis@yahoo.de', // list of receivers
+          subject: 'Verify your email✔', // Subject line
+          html: `<b>Hello Dennis! <br>
+           Please click on following link to verify your email: <a href="http://localhost:3000/signupVerification/${token}">
+           http://localhost:3000/signupVerification/${token}</a></b>`,
+        })
+        .catch((error) => {
+          console.log('Fehler beim Senden der Email ' + error);
+        });
     });
 
     res.render('checkYourEmail');
